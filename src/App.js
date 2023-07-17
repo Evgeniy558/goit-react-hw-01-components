9e4360f6b252f5190ee5043dfd49fc50f4a1647d
Evgenij Prishchepov <prishchepovee@gmail.com>
2023-07-17 12:00:42 +0300

    1

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import css from "./App.module.css";
+import { Profile } from "./components/Profile";
+import user from "./components/Profile/user.json";
+
+function App() {
+  return (
+    <div className={css.container}>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </div>
+  );
+}
+
+export default App;
